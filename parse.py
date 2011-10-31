c05d1aaab329ffb542214f017ff7e9e8640ac2a6
Frédéric Sagnes <speedup@gmail.com>
2011-10-31 23:43:12 +0100

    Go pro: split the hacky script in functions, run only form the command line

--- parse.py
+++ parse.py
@@ -20,65 +20,82 @@ MONTHS = {
     "November": 11,
     "December": 12
 }
-LINK_START_TAG = '<a href="StoryView.py?'
-LINK_END_TAG = '">'
 
-stories_filenames = set()
 
-for offset in [0, 60]:
-    data = None
-    cache_filename = 'index-%d.html' % offset
-
-    if not os.path.exists(cache_filename):
-        print 'Downloading stories with offset %d' % offset
-        data = urllib2.urlopen('http://www.folklore.org/ProjectView.py?project=Macintosh&index=%d&detail=low' % offset).read()
-
-        with open(cache_filename, 'w') as cache:
-            cache.write(data)
-    else:
-        with open(cache_filename) as cache:
-            data = cache.read()
-
+def fetch_story_links(data):
     # Look for <a href="StoryView.py?project=Macintosh&story=I'll_Be_Your_Best_Friend.txt&sortOrder=Sort by Date&detail=low">
-    print 'Parsing stories with offset %d' % offset
-    start = data.find(LINK_START_TAG)
+    start_tag = '<a href="StoryView.py?'
+    end_tag = '">'
+    stories_filenames = set()
+    start = data.find(start_tag)
 
     while start != -1:
         data = data[start:]
-        end = data.find(LINK_END_TAG)
-        link = urlparse.parse_qs(data[len(LINK_START_TAG):end])
+        end = data.find(end_tag)
+        link = urlparse.parse_qs(data[len(start_tag):end])
 
         if link['project'][0] != 'Macintosh':
             raise Exception('Stories should be about Macintosh')
 
         stories_filenames.add(link['story'][0])
         data = data[end:]
-        start = data.find(LINK_START_TAG)
+        start = data.find(start_tag)
 
-print 'Found %d stories' % len(stories_filenames)
-stories = []
+    return stories_filenames
 
-for story_filename in stories_filenames:
-    data = None
 
-    if not os.path.exists(story_filename):
-        url = 'http://www.folklore.org/projects/Macintosh/%s' % urllib.quote(story_filename)
-        print 'Downloading %s' % story_filename
-        data = urllib2.urlopen(url).read()
+def fetch_stories():
+    stories_filenames = set()
+    offset = 0
 
-        with codecs.open(story_filename, 'w', 'utf8') as story_file:
-            story_file.write(data)
-    else:
-        with codecs.open(story_filename, 'r', 'utf8') as story_file:
-            data = story_file.read()
+    while True:
+        data = None
+        cache_filename = 'index-%d.html' % offset
 
-    data = data.replace('\r\n', '\n')
+        if not os.path.exists(cache_filename):
+            print 'Downloading stories with offset %d' % offset
+            data = urllib2.urlopen('http://www.folklore.org/ProjectView.py?project=Macintosh&index=%d&detail=low' % offset).read()
 
-    print 'Parsing %s' % story_filename
+            with codecs.open(cache_filename, 'w', 'utf8') as cache:
+                cache.write(data)
+        else:
+            with codecs.open(cache_filename, 'r', 'utf8') as cache:
+                data = cache.read()
+
+        page_stories_filenames = fetch_story_links(data)
+
+        if stories_filenames.issuperset(page_stories_filenames):
+            break
+        else:
+            offset += 60
+            stories_filenames.update(fetch_story_links(data))
+
+    return stories_filenames
+
+
+def parse_story_content(content):
+    html_content_lines = []
+
+    for line in content.split('\n'):
+        stripped_line = line.strip()
+
+        if stripped_line != '':
+            # [image:polaroids/polaroids.1.jpg]
+            # [image:bomb_icon.jpg::hspace=12 vspace=12 align=left]
+            # [story:Round Rects Are Everywhere!]
+            # [image:early_macpaint.jpg:An early screenshot of a half-implemented MacPaint]
+            # [image:mousepaint.jpg:Bill Budge's MousePaint:align=left hspace=8]
+            # [link:here.:http//www.amazon.com/exec/obidos/asin/0596007191/ref=nosim/folklore-20]
+            html_content_lines.append('<p>%s</p>' % stripped_line)
+
+    return '\n\n'.join(html_content_lines)
+
+
+def parse_story(data):
     story = {}
+    image_filenames = set()
     header, content = data.split('\n\n', 1)
 
-    # Look for Project: Macintosh
     for line in header.split('\n'):
         key, value = line.split(': ', 1)
         story[key] = value
@@ -97,11 +114,47 @@ for story_filename in stories_filenames:
 
     for key in ['Characters', 'Topics']:
         if story.has_key(key):
-            story[key] = story[key].split(',')
+            story[key] = frozenset(story[key].split(','))
 
     if story.has_key('Image'):
-        image_filename = story['Image']
+        image_filenames.add(story['Image'])
 
+    story['Content'] = content
+    story['HTMLContent'] = parse_story_content(content)
+
+    return story, image_filenames
+
+
+def parse_stories(stories_filenames):
+    stories = []
+    image_filenames = set()
+
+    for story_filename in stories_filenames:
+        data = None
+
+        if not os.path.exists(story_filename):
+            url = 'http://www.folklore.org/projects/Macintosh/%s' % urllib.quote(story_filename)
+            print 'Downloading %s' % story_filename
+            data = urllib2.urlopen(url).read()
+
+            with codecs.open(story_filename, 'w', 'utf8') as story_file:
+                story_file.write(data)
+        else:
+            with codecs.open(story_filename, 'r', 'utf8') as story_file:
+                data = story_file.read()
+
+        data = data.replace('\r\n', '\n')
+
+        story, story_image_filenames = parse_story(data)
+        image_filenames.update(story_image_filenames)
+
+        stories.append(story)
+
+    return stories, image_filenames
+
+
+def download_images(image_filenames):
+    for image_filename in image_filenames:
         if not os.path.exists(image_filename):
             print 'Downloading %s' % image_filename
 
@@ -109,24 +162,16 @@ for story_filename in stories_filenames:
                 url = 'http://folklore.org/projects/Macintosh/images/%s' % urllib.quote(image_filename)
                 image_file.write(urllib2.urlopen(url).read())
 
-    html_content_lines = []
 
-    for line in content.split('\n'):
-        stripped_line = line.strip()
+def build_book():
+    stories_filenames = fetch_stories()
+    print 'Found %d stories' % len(stories_filenames)
+    stories, image_filenames = parse_stories(stories_filenames)
+    download_images(image_filenames)
 
-        if stripped_line != '':
-            # [image:polaroids/polaroids.1.jpg]
-            # [image:bomb_icon.jpg::hspace=12 vspace=12 align=left]
-            # [story:Round Rects Are Everywhere!]
-            # [image:early_macpaint.jpg:An early screenshot of a half-implemented MacPaint]
-            # [image:mousepaint.jpg:Bill Budge's MousePaint:align=left hspace=8]
-            # [link:here.:http//www.amazon.com/exec/obidos/asin/0596007191/ref=nosim/folklore-20]
-            html_content_lines.append('<p>%s</p>' % stripped_line)
+    for story in sorted(stories, key=lambda story: story['ParsedDate']):
+        print ' * %s (%s)' % (story['Title'], story['Date'])
 
-    story['Content'] = content
-    story['HTMLContent'] = '\n\n'.join(html_content_lines)
 
-    stories.append(story)
-
-for story in sorted(stories, key=lambda story: story['ParsedDate']):
-    print '%s (%s)' % (story['Title'], story['Date'])
+if __name__ == '__main__':
+    build_book()
